feb6f55ea5d06fef50284d8b583faa62545def81
Szymon Chyc <erressrsch@gmail.com>
2024-01-12 23:01:52 +0100

    Add project files.

---
+++ mod2_les8_task5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");
+        }
+    }
+}
